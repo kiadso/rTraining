825860c8e60338afb354c7f9099510ab120ec8fa
Genelle Denzin <genelle@eyedarts.net>
2020-06-13 13:31:47 -0400

    totally changed the order and focused content a little

--- Week2Demo.R
+++ Week2Demo.R
@@ -1,23 +1,35 @@
-# Settings to Change First Thing
-
-# 
 
 # You can run R code in RGUI, which comes with your R installation. (Demo quickly)
 
-# RStudio gives you the space to organize your code, connect to data sources, 
-# iterate, share, and back up your code. Makes coding easier.
+# History -----------------------------------------------------------------
+
+# The commands you submitted last week **are** actually available to you if you 
+# do want those.
+
+# Project-based Workflows -------------------------------------------------
+
+# WHY
+# - Highly recommended as a way to keep your work more reproducible
+# - Useful as a way of always having a relevant working directory
+# - Keep like work together
+# - Can have multiple projects open simultaneously
+
+# LET'S CREATE ONE
+# - File > New Project > New Directory
+# - Name it "practice"
+# - Make the Directory say "~\R" and don't check anything
+# - Click Create
+# - Examine Files tab
+
+# Scripts -----------------------------------------------------------------
 
 # When we left off last week, we had only written some R commands in the 
-# console and then closed RStudio, thereby (kind of) losing the work we did. 
-# And it was ok because we were simply having a conversation with R, learning 
-# to feel ok in the console. Also it was ok because the code we used during 
-# that session is available on GitHub, which is the topic of next week's
-# lesson.
+# console and then closed RStudio. What if we had wanted to save that code so 
+# we could use or share it later?
 
-# But what if we had wanted to save that code so we could use it later or share it?
+# CREATE A SCRIPT
 
-# Open RStudio, create script, and type the following into the console, (while 
-# teaching as you go).
+# - File > New File > R Script
 
 # A data frame is a 2 dimensional data structure in R
 # Columns = variables, rows = observations
@@ -47,7 +59,69 @@ plot(mtcars$mpg, mtcars$carb)
 
 hist(mtcars$mpg)
 
+write.csv(attributes_mtcars, file = "Week2Script.csv")
+
 # save script
 
+# search script
 
+# Sessions, workspaces ----------------------------------------------------
 
+ls()
+# - Restart R
+# - Clear Environment
+
+# Installing Packages -----------------------------------------------------
+
+install.packages("dplyr")
+
+library(dplyr)
+
+# how and when to update packages: Tools > Check for Package Updates...
+
+# I update packages weekly. It has set me back some troubleshooting only twice 
+# in 2 yrs. Also good to keep R and RStudio updated.
+
+# Settings to Change Now --------------------------------------------------
+
+# Tools > Global Settings > General > Workspace > uncheck "Restore .RData into 
+# workspace at startup" and set "Save Workspace" to "Never"
+
+# Why: You should save code that works every time. Write every script assuming 
+# it will be run in a fresh R process. 
+
+# A great resource about workflows is https://rstats.wtf (What They Forgot... 
+# to teach you)
+
+# Absolutely cool things if there's time ----------------------------------
+
+# data copied in from random titanic dataset
+
+# vertical cursor demo
+
+sex	age
+female	29
+male	0.92
+female	2
+male	30
+female	25
+male	48
+female	63
+male	39
+female	53
+male	71
+male	47
+female	18
+female	24
+female	26
+male	80
+male	
+male	24
+female	50
+female	32
+male	36
+male	37
+female	47
+male	26
+
+# Navigating a script
