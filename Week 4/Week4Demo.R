2b6c35b9cea697e9f6b9b3c4d13dfaa61a556949
Genelle Denzin <genelledenzin@cohhio.org>
2020-06-26 10:15:33 -0500

    adding content

--- Week 4/Week4Demo.R
+++ Week 4/Week4Demo.R
@@ -1,38 +1,14 @@
-# In terminal, tell RStudio your git credentials
-# $ git config --global user.name "Your Name Comes Here"
-# $ git config --global user.email you@yourdomain.example.com
 
+# In practice project, create an R Markdown file
 
-# Log into GitHub
-# Create new repository
-# (talk about licensing)
-# Clone (copy link)
-# Go to RStudio
-# Create a project (version control this time)
-# Paste link
-# Create
-## Does everyone have a Git tab????
-# Create a script, save an initial comment or something to it
-# Diff
-# Commit
-# Commit message
-# Push
-# Go look at GitHub
-# add to .gitignore (and explain why)
-# talk about the readme.md
+# Show students around the examples already there, cover:
+# code chunk
+# Knit button
+# Ctrl-enter to run a single line in a chunk
+# YAML output to html, word, ppt, pdf
 
-
-# (talk about incorporating other computers/co-workers into
-# your repo)
-# Share Happy With Git as a resource
-
-# Navigate to the repo for this training project
-# Clone/copy link for it
-# Create new project using this link
-# NOW EVERYONE HAS ALL THE MATERIALS!!!
-
-# Touring GitHub repository:
-# Projects
-# Organizations
-# Issues
-# Network graphic
+# delete everything under the setup chunk
+# change header to "HMIS Participating Organizations"
+# adjust default code chunk settings
+# call libraries (install gt package)
+# 
